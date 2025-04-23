e4d258b1b43ee8902fabbc077a99fbe880428738
Luiz Guilherme <luizguilherme011@gmail.com>
2025-04-22 22:23:14 -0300

    Update

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/ping', (req, res) => {
-  res.json({ message: 'pong' });
-});
-
-module.exports = router;
